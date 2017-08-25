5f251b85ed81d2c8791b35991501fbbd10d5469f
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-08-25 15:08:40 +0200

    Using GetMapping

--- demo01/number-api/src/main/java/org/agoncal/talk/msexperience/demo01/numberapi/rest/IsbnResource.java
+++ demo01/number-api/src/main/java/org/agoncal/talk/msexperience/demo01/numberapi/rest/IsbnResource.java
@@ -1,14 +1,14 @@
 package org.agoncal.talk.msexperience.demo01.numberapi.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 @RestController
+@RequestMapping("/api")
 public class IsbnResource {
 
-    @RequestMapping(method = GET, path = "/api/isbn")
+    @GetMapping("/isbn")
     public String generateIsbnNumber() {
         return "ISBN-" + Math.random();
     }
