08aa5442efa8afd013a4f3911d05fee8bbab7ad3
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-08-25 10:58:30 +0200

    Adding 1st draft of Spring boot

---
+++ demo01/number-api/src/main/java/org/agoncal/talk/msexperience/demo01/numberapi/NumberApiApplication.java
@@ -0,0 +1,12 @@
+package org.agoncal.talk.msexperience.demo01.numberapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberApiApplication.class, args);
+    }
+}
