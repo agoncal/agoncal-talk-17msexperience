5bd737148a79e0e6fc2d211d4ade986a8e950c34
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-09-30 22:07:40 +0200

    Changing Feign URL

--- demo03/book-api/src/main/java/org/agoncal/talk/msexperience/demo03/bookapi/client/isbn/api/ApiApiClient.java
+++ demo03/book-api/src/main/java/org/agoncal/talk/msexperience/demo03/bookapi/client/isbn/api/ApiApiClient.java
@@ -1,8 +1,7 @@
 package org.agoncal.talk.msexperience.demo03.bookapi.client.isbn.api;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.agoncal.talk.msexperience.demo03.bookapi.client.isbn.ClientConfiguration;
 
-@FeignClient(name="${isbn.name:isbn}", url="${isbn.url:http://localhost:8081}", configuration = ClientConfiguration.class)
+@FeignClient(value = "numberapi")
 public interface ApiApiClient extends ApiApi {
 }
